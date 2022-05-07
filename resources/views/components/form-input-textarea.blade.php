647d0e9a58af9982254ad18e0e27550479e62657
mess-dev <stephane.maissin@gmail.com>
2022-05-07 05:07:21 +0100

    init

--- resources/views/components/form-input-textarea.blade.php
+++ resources/views/components/form-input-textarea.blade.php
@@ -10,9 +10,7 @@
 			name="{{ Str::replace('-', '_', $id) }}"
 			placeholder="{{ $placeholder }}"
 			{{ isset($disabled) ? 'disabled' : '' }}
-			class="ckeditor rounded-md shadow-sm border-gray-300 border text-lg focus:border-yellow-300 focus:ring focus:ring-yellow-200 focus:ring-opacity-50 px-2 py-1 form-textarea">
-				{{$old}}
-	</textarea>
+			class="rounded-md shadow-sm border-gray-300 border text-lg focus:border-yellow-300 focus:ring focus:ring-yellow-200 focus:ring-opacity-50 px-2 py-1 form-textarea">{{$old}}</textarea>
 	@if(isset($errors) && $errors->has(Str::replace('-', '_', $id)))
 		<div class="alert alert-danger text-red-500">{{ $errors->first(Str::replace('-', '_', $id)) }}</div>
 	@endif
--- resources/views/components/seo-meta.blade.php
+++ resources/views/components/seo-meta.blade.php
@@ -1,12 +1,10 @@
 {!! Meta::toHtml() !!}
+
 <!-- Fonts -->
 <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 <style>
 	body {
 		font-family: 'Nunito', sans-serif;
-	}
-	.page-break {
-		page-break-after: always;
 	}
 </style>
 
@@ -14,8 +12,6 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
 	  integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ=="
 	  crossorigin="anonymous" referrerpolicy="no-referrer"/>
-
-<script src="{{asset('ckeditor/ckeditor.js')}}"></script>
 
 <!-- Scripts -->
 <script src="{{ mix('js/app.js') }}" defer></script>
